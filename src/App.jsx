a53d560ef656e3a058a56813d275e0672e755bcc
Jamat Ali <engr.jamatali@gmail.com>
2024-01-17 22:40:28 +0500

    Vite Code Removal and Tailwindcss configured and tested

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="grid grid-col-12 text-center bg-slate-300 px-20">
+        <h1>React Redux</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
