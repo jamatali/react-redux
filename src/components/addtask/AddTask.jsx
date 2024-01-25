ecc780d28f766cf0892c7f5556274b2ec84fb8dc
Jamat Ali <engr.jamatali@gmail.com>
2024-01-25 10:58:31 +0500

    justify

--- src/components/addtask/AddTask.jsx
+++ src/components/addtask/AddTask.jsx
@@ -6,14 +6,17 @@ function AddTask() {
       <div className="grid border bg-blue-400 justify-center">
         <div className="col-span-6 border bg-red-400">
           <form className="border">
-            <label className="">Blog Title</label>
-            <input
-              className=""
-              type="text"
-              placeholder="Blog Title"
-              name="title"
-            />
-            <br />
+            <div className="grid justify-between">
+              <div className="p-4 col-span-1 bg-black"></div>
+              <div className="p-4 col-span-1 bg-red-500"></div>
+              {/* <label className="col-span-1">Blog Title</label>
+              <input
+                className="col-span-1"
+                type="text"
+                placeholder="Blog Title"
+                name="title"
+              /> */}
+            </div>
             <label className="col-span-2">Author Name</label>
             <input
               className="col-span-10 bg-slate-200"
